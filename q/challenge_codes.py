71b9dd2f93e1fe2fb0f6e9508006de0670eccad1
uiandwe <uiandwe@gmail.com>
2016-07-24 20:09:50 +0900

    제일 작은 수 제거하기

--- q/challenge_codes.py
+++ q/challenge_codes.py
@@ -26,3 +26,16 @@ def number_generator(x, n):
 
 def hide_numbers(s):
     return "*"*(len(s)-4)+s[len(s)-4:]
+
+
+'''
+제일 작은 수 제거하기
+rm_small함수는 list타입 변수 mylist을 매개변수로 입력받습니다.
+mylist 에서 가장 작은 수를 제거한 리스트를 리턴하고, mylist의 원소가 1개 이하인 경우는 []를 리턴하는 함수를 완성하세요.
+예를들어 mylist가 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10, 8, 22]면 [10, 22]를 리턴 합니다.
+'''
+
+
+def rm_small(mylist):
+    mylist.pop(mylist.index(min(mylist)))
+    return mylist
