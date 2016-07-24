fe2d51aca8c3803aa3c009458e96281f4444e3bf
uiandwe <uiandwe@gmail.com>
2016-07-24 20:45:39 +0900

    문자열 내 마음대로 정렬하기

--- q/challenge_codes.py
+++ q/challenge_codes.py
@@ -58,3 +58,16 @@ def nextSqure(n):
         return int(root + 1) ** 2
     else:
         return "no"
+
+
+'''
+문자열 내 마음대로 정렬하기
+strange_sort함수는 strings와 n이라는 매개변수를 받아들입니다.
+strings는 문자열로 구성된 리스트인데, 각 문자열을 인덱스 n인 글자를 기준으로 정렬하면 됩니다.
+
+예를들어 strings가 ["sun", "bed", "car"]이고 n이 1이면 각 단어의 인덱스 1인 문자 u, e ,a를 기준으로 정렬해야 하므로 결과는 ["car", "bed", "sun"]이 됩니다.
+strange_sort함수를 완성해 보세요.
+'''
+
+def strange_sort(strings, n):
+    return sorted(strings, key=lambda x: x[n])
