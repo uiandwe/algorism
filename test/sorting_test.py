ac39552e512df5a1446caaf62dd4c396dc09939b
uiandwe <uiandwe@gmail.com>
2016-08-29 20:45:08 +0900

    add test file

--- test/sorting_test.py
+++ test/sorting_test.py
@@ -8,5 +8,12 @@ import sorting.sorting
 
 def test_bubble():
     temp_list = [9, 2, 6, 1, 8, 10, 4, 5, 3, 7]
-    s = sorting.sorting()
-    print(s.bubble(temp_list))
+    s = sorting.sorting.sorting()
+    assert s.bubble(temp_list) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+@xfail
+def test_bubble_fail():
+    temp_list = [9, 2, 6, 1, 8, 10, 4, 5, 3, 7]
+    s = sorting.sorting.sorting()
+    assert s.bubble(temp_list) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
