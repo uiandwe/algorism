4de588134b125254c48e5b80c21d4958aaa3fb09
uiandwe <uiandwe@gmail.com>
2016-09-28 23:32:33 +0900

    coin change

--- q/cc.py
+++ q/cc.py
@@ -12,3 +12,8 @@ if __name__ == '__main__':
         d.append(0)
     d[0] = 1
 
+    for i in reversed(range(cn)):
+        for j in range(coin[i], money+1):
+            d[j] += d[j-coin[i]]
+
+    print(d[money])
