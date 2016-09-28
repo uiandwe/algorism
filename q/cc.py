1d4c1c2d2ab0749f9e7c5c16c518ed38ab95b2a6
uiandwe <uiandwe@gmail.com>
2016-09-28 23:25:09 +0900

    cc init

---
+++ q/cc.py
@@ -0,0 +1,14 @@
+__author__ = 'root'
+
+
+if __name__ == '__main__':
+    cn = 4
+    coin = [1, 2, 5, 10]
+
+    money = 20
+
+    d = []
+    for i in range(21):
+        d.append(0)
+    d[0] = 1
+
